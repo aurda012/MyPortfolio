d671b4aac19bb97767b0ee8db4efb952d10cb412
Alfredo <alfredo.i.urdaneta@gmail.com>
2017-03-18 20:40:42 -0400

    Redo of implemting initial file upload system via carrierwave

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,7 +1,7 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
 
-  storage :aws
-  
+  storage :file
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
