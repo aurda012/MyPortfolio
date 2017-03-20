a98980cb6fd42eb282d2f870c3490ff748ace3e5
Alfredo <alfredo.i.urdaneta@gmail.com>
2017-03-19 22:55:21 -0400

    Implemented action cable for comments

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,7 +17,7 @@ class BlogsController < ApplicationController
     @comment = Comment.new
 
     @page_title = @blog.title
-    @seo_keywords = @blog.body 
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
@@ -36,7 +36,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
         format.html { render :new }
       end
@@ -60,18 +60,19 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
+      format.json { head :no_content }
     end
   end
-  
+
   def toggle_status
-    if @blog.draft? 
+    if @blog.draft?
       @blog.published!
     elsif @blog.published?
       @blog.draft!
     end
-      
-    redirect_to blogs_url, notice: "Post status has been updated."
+        
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.required(:comment).permit(:content)
+    params.require(:comment).permit(:content)
   end
 end
