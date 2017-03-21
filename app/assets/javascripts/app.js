9e21cab03e2eebd31ef39196ecc2885ccb9066af
Alfredo <alfredo.i.urdaneta@gmail.com>
2017-03-21 09:14:29 -0400

    Added new index for pages and new javascript

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,32 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
-//= require html.sortable
-//= require cocoon
-//= require gritter
-//= require cable
-//= require angular
-//= require angular-animate
-//= require nprogress
-//= require nprogress-ajax
-//= require nprogress-angular
-//= require nprogress-turbolinks
-//= require turbolinks
-//= require_tree .
-//= require bootstrap-sprockets
-
 function Trigger(top, height){
   this.top = top;
   this.height = height;
@@ -260,7 +231,7 @@ $(document).ready(function (){
 
 
   var data = {
-    labels: ["HTML", "JavaScript", "CSS", "Machine Learning", "Ruby On Rails", "SQL"],
+    labels: ["HTML", "JavaScript", "CSS", "Angular", "Ruby On Rails", "UX/UI"],
     datasets: [
         {
             label: "Skills",
@@ -280,7 +251,7 @@ $(document).ready(function (){
               'rgba(0, 0, 0, .2)',
               'rgba(0, 0, 0, .2)'
             ],
-            data: [80, 50, 80, 55, 85, 60, 0],
+            data: [80, 60, 80, 50, 85, 65, 0],
         }
     ]
   };
